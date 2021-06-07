e292d2e574cae6e1feb69b688ab4d6547d0f774b
alanmdf <alanmoreirascr@gmail.com>
2021-06-07 16:40:13 -0300

    Requisito Obrigatorio 2 Concluido

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 window.onload = function() {
   const inputText = document.getElementById('text-input');
   const inputParagraph = document.getElementById('meme-text');
+  const imgElement = document.getElementById('meme-image');
+  const inputImage = document.getElementById('meme-insert');
 
   inputText.addEventListener('keyup', generateText);
+  inputImage.addEventListener('change', displayImage);
 
   function generateText() {
     inputParagraph.innerText = inputText.value;
+  }
+
+  function displayImage(event) {
+    imgElement.src = URL.createObjectURL(event.target.files[0]);
   }
 }
